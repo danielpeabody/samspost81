efe2753953a687922ca60ec0d7350e9825d1230f
danielpeabody <peabodydaniel1@gmail.com>
2022-07-30 09:13:34 -0700

    feat: add full navigation

--- script.js
+++ script.js
@@ -3,6 +3,11 @@
 
 let hamburger = document.querySelector(".fa-solid");
 let navmenu = document.querySelector(".navbar__links");
+let navcontainer = document.querySelector(".navbar__container");
+let navbar = document.querySelector(".navbar");
+
+let screenwith = window.matchMedia("(min-width: 900px)");
+
 
 hamburger.addEventListener("mouseover",()=>{
     hamburger.style.cursor = "pointer";
@@ -10,4 +15,16 @@ hamburger.addEventListener("mouseover",()=>{
 
 hamburger.addEventListener("click",()=>{
     navmenu.classList.toggle("hide");
+})
+
+window.addEventListener("resize",()=>{
+    if(screenwith.matches){
+        navmenu.classList.remove("hide");
+        navcontainer.appendChild(navmenu);
+    }
+    else {
+        navmenu.classList.add("hide");
+        navcontainer.removeChild(navmenu);
+        navbar.appendChild(navmenu);
+    }
 })
